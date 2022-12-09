6fd53c91b9b1075e86b6e4213cc27f1bbe807ff7
Seung Joon Rhee <sj78sj78@gmail.com>
2022-12-09 15:36:15 -0500

    Fix style

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const express = require('express')
-const path = require('path')
-const request = require("request");
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const data = require(path.join(__dirname, 'data.json'));
+const PORT = process.env.PORT || 5000;
 const app = express();
-var data = require(path.join(__dirname, 'data.json'));
 
 app.use(express.static(path.join(__dirname, 'assets')));
 app.set('view engine', 'ejs');
